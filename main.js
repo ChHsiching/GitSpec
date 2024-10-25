a2bc09bfa8cc5818f759266c15e5b6090f557f11
ChHsiching <hsichingchang@gmail.com>
2024-10-25 10:46:54 +0800

    ✨ Feat: add preload script to BrowserWindow
    Attached a preload script to the BrowserWindow constructor to enable access to certain Node.js features in the renderer process. The preload script helps expose specific functionality from the main process to the renderer.

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
 
 const createWindow = () => {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     // and load the index.html of the app.
